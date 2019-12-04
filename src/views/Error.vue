2cc9ba97816d2385925770d9ac04d3314c48da09
Touch <s.chantouch@mango-byte.com>
2019-12-04 22:00:30 +0700

    add error component

---
+++ src/views/Error.vue
@@ -0,0 +1,20 @@
+<template>
+    <div v-if="$validator.has(input)" class="alert-error" :class="inputClass">
+        {{ $validator.first(input) }}
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            input: {
+                type: String,
+                required: true
+            },
+            inputClass: {
+                type: [String, Object],
+                required: false
+            }
+        },
+        name: 'error-message',
+    };
+</script>
