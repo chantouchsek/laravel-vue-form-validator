6eeea9ea831b13c4c216378c77fba1f2c16e99be
Touch <s.chantouch@mango-byte.com>
2019-12-06 09:13:03 +0700

    remove some

--- src/index.js
+++ src/index.js
@@ -1,34 +1,30 @@
-import Validator from './Validator'
-import ErrorComponent from './ErrorComponent'
-import axios from 'axios'
+import Validator from './Validator';
+import axios from 'axios';
 
 class FormValidator {
     install (Vue) {
-        Vue.component('error', ErrorComponent);
-        if (axios) {
-            axios.interceptors.response.use((response) => {
-                return response;
-            }, (error) => {
-                if (error.response.status === 422) {
-                    Validator.fill(error.response.data.errors)
-                }
-                return Promise.reject(error);
-            });
-        }
+        axios.interceptors.response.use((response) => {
+            return response;
+        }, (error) => {
+            if (error.response.status === 422) {
+                Validator.fill(error.response.data.errors);
+            }
+            return Promise.reject(error);
+        });
         Vue.mixin({
             beforeCreate () {
                 this.$options.$errors = {};
                 Vue.util.defineReactive(this.$options, '$errors', Errors);
                 if (!this.$options.computed) {
-                    this.$options.computed = {}
+                    this.$options.computed = {};
                 }
-                this.$options.computed["$errors"] = function () {
+                this.$options.computed['$errors'] = function() {
                     return this.$options.$errors;
                 };
             },
-        })
+        });
     }
 }
 
-export { default as Validator } from './Validator'
-export default new FormValidator()
+export { default as Validator } from './Validator';
+export default new FormValidator();
