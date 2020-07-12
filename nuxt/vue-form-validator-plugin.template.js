d9ce34f840454da87986fbd1d18649901f1550f2
Touch <s.chantouch@mango-byte.com>
2020-07-12 13:23:35 +0700

    intance

--- nuxt/vue-form-validator-plugin.template.js
+++ nuxt/vue-form-validator-plugin.template.js
@@ -1,12 +1,12 @@
 import Vue from 'vue';
-import FormValidator, { BaseProxy } from 'laravel-vue-form-validator';
+import FormValidator, { BaseProxy, Validator } from 'laravel-vue-form-validator';
 
 Vue.use(FormValidator);
 
 export default ({ app, $axios }) => {
     const [pluginOptions] = [<%= serialize(options) %>]
     const { baseUrl } = pluginOptions || {}
-    app.$errors = FormValidator;
+    app.$errors = Validator;
     BaseProxy.$http = $axios;
     if (pluginOptions && baseUrl) {
         BaseProxy.$baseUrl = baseUrl
